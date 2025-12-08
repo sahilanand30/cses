a26da38abb95dabd0f2943fe597e9bf50de1992c
sahilanand30 <sahilhanand@gmail.com>
2025-12-09 02:19:56 +0530

    bit string

---
+++ Introductory Problems/bit_strings.cpp
@@ -0,0 +1,39 @@
+/*
+Author: Sahil Anand
+Find me on: https://linktr.ee/sahilanand30
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define nl endl
+
+/*-------------------------{Main Code}-------------------------*/
+ll binpow(ll a, ll b, ll M)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % M;
+        a = (a * a) % M;
+        b >>= 1;
+    }
+    return res;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << binpow(2LL, n, mod) << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
