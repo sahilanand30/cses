92aa49a9301a4b3c8e348abb712060b178ca665f
sahilanand30 <sahilhanand@gmail.com>
2025-12-09 20:23:12 +0530

    coin piles

---
+++ Introductory Problems/coin_piles.cpp
@@ -0,0 +1,35 @@
+/*
+Author: Sahil Anand
+Find me on: https://linktr.ee/sahilanand30
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define nl endl
+
+/*-------------------------{Main Code}-------------------------*/
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (a > b)
+        swap(a, b);
+    if (b > a + a)
+        cout << "NO\n";
+    else if ((a + b) % 3 == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
