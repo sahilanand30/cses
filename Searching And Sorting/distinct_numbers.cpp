1b9cffeb6c116dd12d5067ff7e88da9e293b8004
sahilanand30 <sahilhanand@gmail.com>
2025-12-11 00:14:45 +0530

    distinct numbers

---
+++ Searching And Sorting/distinct_numbers.cpp
@@ -0,0 +1,34 @@
+/*
+Author: Sahil Anand
+Find me on: https://linktr.ee/sahilanand30
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define nl endl
+
+/*-------------------------{Main Code}-------------------------*/
+
+void solve()
+{
+    ll n, x;
+    cin >> n;
+    set<ll> st;
+    while (n--)
+    {
+        cin >> c;
+        st.insert(x);
+    }
+    cout << st.size() << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
