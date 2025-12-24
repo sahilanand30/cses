3a2f1c02555084ffeaa0630afba9ad5f4087058a
sahilanand30 <sahilhanand@gmail.com>
2025-12-24 23:40:50 +0530

    subarray sums 2

---
+++ Searching And Sorting/subarray_sums_2.cpp
@@ -0,0 +1,36 @@
+/*
+Author: Sahil Anand
+Find me on: https://linktr.ee/sahilanand30
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define nl endl
+/*-------------------------{Main Code}-------------------------*/
+
+void solve()
+{
+    ll n, x, a, s = 0, ans = 0;
+    cin >> n >> x;
+    map<ll, ll> m;
+    m[0]++;
+    while (n--)
+    {
+        cin >> a;
+        s += a;
+        ans += m[s - x];
+        m[s]++;
+    }
+    cout << ans << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
